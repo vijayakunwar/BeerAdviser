7879a1cc1ab54133078443f6403dc005a3d25dd6
vijayakunwar <kunwar.vijaya@gmi>
2020-03-01 22:42:04 +1000

    spelling error in README.md file

--- app/src/main/java/com/hfad/MainActivity.java
+++ app/src/main/java/com/hfad/MainActivity.java
@@ -7,9 +7,11 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     //comment test for github commit
+    //do commenting on project file has contribution
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
